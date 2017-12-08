6aa98714b1c1aed84a54adaffc76a7ae3ad7bcf9
Maryadi <maryadi@astrnt.co>
2017-12-08 10:35:31 +0700

    add cron running style hehe

--- app.js
+++ app.js
@@ -2,4 +2,44 @@ var CronJob = require('cron').CronJob;
 
 new CronJob('* * * * * *', function() {
   console.log('You will see this message every second');
-}, null, true, 'Asia/Jakarta);
+}, null, true, 'Asia/Jakarta');
+
+/**********************************************************/
+var job = new CronJob('00 30 11 * * 1-5', function() {
+  /*
+   * Runs every weekday (Monday through Friday)
+   * at 11:30:00 AM. It does not run on Saturday
+   * or Sunday.
+   */
+
+   console.log("run on weekday ajjah");
+  }, function () {
+    /* This function is executed when the job stops */
+  },
+  true, /* Start the job right now */
+  'Asia/Jakarta' /* Time zone of this job. */
+);
+////////////////////////////////////////////////////////////////
+
+
+
+
+new CronJob('00 00 00 * * 1-5', function() {
+  /*
+   * Runs every weekday (Monday through Friday)
+   * at 00:00:00 AM. It does not run on Saturday
+   * or Sunday.
+   */
+
+   console.log("run on weekday ajjah pukul 00.00");
+  }, function () {
+    /* This function is executed when the job stops */
+  },
+  true, /* Start the job right now */
+  'Asia/Jakarta' /* Time zone of this job. */
+);
+
+
+// new CronJob('* * * * * *', function() {
+//   console.log('xxxxxxxxxxxxxxxxx');
+// }, null, true, 'Asia/Jakarta');
